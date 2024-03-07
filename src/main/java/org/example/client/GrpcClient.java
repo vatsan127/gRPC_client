bb9c100f84f6f27923b82d797f9c489f2f5dbf9a
Srivatsan <110617159+vatsan127@users.noreply.github.com>
2024-03-07 15:17:15 +0530

    commit for gRPC client /c/Git/gRPC_client

--- src/main/java/org/example/client/GrpcClient.java
+++ src/main/java/org/example/client/GrpcClient.java
@@ -5,6 +5,8 @@ import io.grpc.ManagedChannelBuilder;
 import org.example.properties.GrpcClientProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.example.client.NumberServiceGrpc;
+import org.example.client.NumberRequest;
 
 import java.util.concurrent.TimeUnit;
 
